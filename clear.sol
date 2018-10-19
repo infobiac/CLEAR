a0f0292954ced53e390c5cf73bf9695986bbc94a
Christophe Rimann <cjr2185@columbia.edu>
2018-10-18 21:19:02 -0400

    OMG GUYSSSSS

---
+++ clear.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.7;
+import "remix_tests.sol"; // this import is automatically injected by Remix.
+
+contract Clear {
+    address whom;
+    string data;
+    uint8 yays;
+    uint8 nays;
+    uint8 pending;
+    mapping(address=>uint8) verifiers;
+    
+    function Clear(string data, address tempwhom, address[] verifier) public {
+        whom = tempwhom;
+        bool is_member = (msg.sender == tempwhom) ? true : false;
+        for(uint8 v = 0; v < verifier.length; v++){
+            if (verifier[v] == msg.sender){
+                is_member = true;
+            }
+            pending++;
+            verifiers[verifier[v]] = 2;
+        }
+    }
+    
+    function attest(bool verify) public{
+        if (verifiers[msg.sender] == 2){
+            verifiers[msg.sender] = verify ? 1 : 0;
+            if(verifiers[msg.sender]==1)
+                yays++;
+            else
+                nays++;
+            pending--;
+        }
+    }
+    
+    function result() public returns(uint8){
+        return yays >= nays ? 1 : 0;
+    }
+    
+    function result_arr() public returns(uint8[3]){
+        return [yays, nays, pending];
+    }
+    
+    function add_verifier(address verifier) public{
+        if(msg.sender != whom)
+            return;
+        verifiers[verifier] = 2;
+    }
+    
+    function get_data() public returns(string){
+        return data;
+    }
+}
+
