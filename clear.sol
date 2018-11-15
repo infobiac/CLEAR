214b39eb93adf78713e8da308354a0055f7821b1
ckuang <ckuang8@gmail.com>
2018-11-15 18:56:25 -0500

    update solidity contract w/ pending and approved

--- clear.sol
+++ clear.sol
@@ -1,53 +1,59 @@
 pragma solidity ^0.4.7;
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 
-contract Clear {
-    address whom;
-    string data;
-    uint8 yays;
-    uint8 nays;
-    uint8 pending;
-    mapping(address=>uint8) verifiers;
-    
-    function Clear(string data, address tempwhom, address[] verifier) public {
-        whom = tempwhom;
-        bool is_member = (msg.sender == tempwhom) ? true : false;
-        for(uint8 v = 0; v < verifier.length; v++){
-            if (verifier[v] == msg.sender){
-                is_member = true;
-            }
-            pending++;
-            verifiers[verifier[v]] = 2;
-        }
-    }
-    
-    function attest(bool verify) public{
-        if (verifiers[msg.sender] == 2){
-            verifiers[msg.sender] = verify ? 1 : 0;
-            if(verifiers[msg.sender]==1)
-                yays++;
-            else
-                nays++;
-            pending--;
-        }
-    }
-    
-    function result() constant public returns(uint8){
-        return yays >= nays ? 1 : 0;
-    }
-    
-    function result_arr() constant public returns(uint8[3]){
-        return [yays, nays, pending];
-    }
-    
-    function add_verifier(address verifier) public{
-        if(msg.sender != whom)
-            return;
-        verifiers[verifier] = 2;
-    }
-    
-    function get_data() constant public returns(string){
-        return data;
-    }
-}
+contract Clarity {
+    address user;
+    string public metadata;
 
+    mapping(address=>string) public verifiers;
+    address[] pending;
+    address[] approved;
+
+    function Clarity(string metadata, address user, string apiURL) public {
+        user = user;
+        metadata = metadata;
+        verifiers[msg.sender] = apiURL;
+    }
+
+    function request_access() public{
+        pending.push(msg.sender);
+    }
+
+    function grant_access(address seeker) public {
+        if (user == msg.sender) {
+            uint idx = 0;
+            bool present = false;
+            for (uint i = 0; i < pending.length; i++){
+                if (pending[i] == seeker) {
+                    idx = i;
+                    present = true;
+                }
+            }
+            if (present) {
+                approved.push(pending[idx]);
+                for (uint y = idx; y < pending.length - 1; y++){
+                    pending[y] = pending[y+1];
+                }
+                delete pending[pending.length-1];
+                pending.length--;
+            }
+        }
+    }
+
+    function getpending() constant public returns(address[]){
+        if (user == msg.sender) {
+            return pending;
+        }
+    }
+
+    function check_access(address seeker) constant public returns(bool){
+        bool present = false;
+        for (uint i = 0; i < pending.length; i++){
+            if (pending[i] == seeker) {
+                present = true;
+            }
+        }
+        return present;
+    }
+
+}
